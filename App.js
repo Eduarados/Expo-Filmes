d4b34abe10eeb45668ecd46bb94f822a6fb2bf6f
betega <2022304810@aluno.riogrande.ifrs.edu.br>
2026-05-18 20:42:18 -0300

    muitofoda1.0

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+
+import {
+  SafeAreaView,
+  FlatList,
+  StyleSheet,
+  Text,
+} from 'react-native';
 
 import Filmes from './components/Filmes';
-
 import api from './services/api';
 
 export default function App() {
 
-  const [movies, setMovies] = useState([]);
+  const [sections, setSections] = useState([]);
 
   useEffect(() => {
     loadMovies();
@@ -17,31 +22,85 @@ export default function App() {
 
     try {
 
-      const response = await api.get('/movie/popular');
+      const [
+        popular,
+        topRated,
+        upcoming,
+        action,
+      ] = await Promise.all([
 
-      setMovies(response.data.results);
+        api.get('/movie/popular'),
 
-      console.log(response.data.results);
+        api.get('/movie/top_rated'),
+
+        api.get('/movie/upcoming'),
+
+        api.get('/discover/movie?with_genres=28'),
+
+      ]);
+
+      setSections([
+
+        {
+          id: '1',
+          title: 'Populares',
+          movies: popular.data.results,
+        },
+
+        {
+          id: '2',
+          title: ' Mais bem avaliados',
+          movies: topRated.data.results,
+        },
+
+        {
+          id: '3',
+          title: 'Próximos lançamentos',
+          movies: upcoming.data.results,
+        },
+
+        {
+          id: '4',
+          title: ' Ação',
+          movies: action.data.results,
+        },
+
+      ]);
 
     } catch (error) {
 
-      console.log(error.response?.data);
-      console.log(error.message);
+      console.log(error);
 
     }
-  }
+  };
 
   return (
 
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
 
       <Text style={styles.header}>
-        Filmes populares
+        🎬 Filmes
       </Text>
 
-      <Filmes movies={movies} />
+      <FlatList
+        data={sections}
+        keyExtractor={(item) => item.id}
 
-    </View>
+        renderItem={({ item }) => (
+
+          <Filmes
+            title={item.title}
+            movies={item.movies}
+          />
+
+        )}
+
+        showsVerticalScrollIndicator={false}
+
+      />
+
+    </SafeAreaView>
+
   );
 }
 
@@ -49,15 +108,16 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 50,
+    backgroundColor: '#121212',
+    paddingTop: 20,
   },
 
   header: {
-    fontSize: 28,
-    textAlign: 'center',
-    marginBottom: 20,
+    color: '#fff',
+    fontSize: 32,
     fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 20,
   },
 
 });
