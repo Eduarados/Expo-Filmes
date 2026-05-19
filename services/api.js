aeed21d6319b75e4df609b3100b765addb602c8f
betega <2022304810@aluno.riogrande.ifrs.edu.br>
2026-05-18 21:10:01 -0300

    agora funfa

--- services/api.js
+++ services/api.js
@@ -4,9 +4,13 @@ const api = axios.create({
 
   baseURL: 'https://api.themoviedb.org/3',
 
-  params: {
-    api_key: 'SUA_API_KEY',
-    language: 'pt-BR',
+  headers: {
+
+    accept: 'application/json',
+
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmVmNzhjZTMyYWZlNTRiOTBhOWJlY2VhMDE1YWZjZiIsIm5iZiI6MTc3NzQwNDU3My4wMDcsInN1YiI6IjY5ZjEwYTlkMGZiYjk0NTNhZDYyYzBjNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.venba2ORZ7uf8xJWW2ji0fPPyfqzt3f5s9WqybHfPrE',
+
   },
 
 });
